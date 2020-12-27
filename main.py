a5eee51bcafb9144caa99b42c6a5fe79afecb5bf
Bruce Wilson <brucewilson1103@gmail.com>
2020-12-27 03:01:46 +0000

    Final game

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure unscathed.") 
-
+print('''
+ 
+*******************************************************************************
+ _                                     _     _                 _ 
+| |                                   (_)   | |               | |
+| |_ _ __ ___  __ _ ___ _   _ _ __ ___ _ ___| | __ _ _ __   __| |
+| __| '__/ _ \/ _` / __| | | | '__/ _ \ / __| |/ _` | '_ \ / _` |
+| |_| | |  __/ (_| \__ \ |_| | | |  __/ \__ \ | (_| | | | | (_| |
+ \__|_|  \___|\__,_|___/\__,_|_|  \___|_|___/_|\__,_|_| |_|\__,_|
+*******************************************************************************
+''')
 choice1 = input('You are walking down a dusty dirt trail when you come to a decaying old oak tree. The trail diverges into two paths. Which route would you like to take? Type "left" or "right" \n').lower()
 if choice1 == "left":
   choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
@@ -10,7 +20,7 @@ if choice1 == "left":
     if choice3 == "red":
       print("It's a room full of flames that consume you and the rest of the structure. Game Over.")
     elif choice3 == "yellow":
-      print("You found the a shimmering chest full of treasure! Congratulations! You Win!")
+      print("You found a shimmering chest full of treasure! Congratulations! You Win!")
       print('''
 *******************************************************************************
           |                   |                  |                     |
@@ -33,7 +43,7 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/_____ /
 *******************************************************************************
 ''')
-      print("You found the a shimmering chest full of treasure! Congratulations! You Win!")
+      print("You found a shimmering chest full of treasure! Congratulations! You Win!")
     elif choice3 == "green":
       print("You enter a room of untamed beasts that attack. Game Over.")
     else:
